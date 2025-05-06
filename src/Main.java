38bd89717bfe172681575a528213c45de5bd24e1
leviutima <levi.utima@gmail.com>
2025-05-06 20:19:06 -0300

    feat:conditions and primitive types

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,44 @@
-public class Main {
-    
-}
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        // declaração de variáveis
+        int minhaIdade = 20;
+        String meuNome = "levi";
+
+        meuNome = "LEVI YUKI UTIMA";
+
+        // var nomeDaMinhaVariavel = "fernanda";
+        
+
+        // tipos primitivos
+        // byte, short, int, long
+
+        // números inteiros
+        // byte => 8bits => -128 a 127
+        // short => 16bits => -32.768 a 32.767
+        // int =>  32bits => 2.147.483.648 a 2.147.483.647
+        // long => 64bits;
+
+        // números decimais
+        // float => 32bits (precisão simples)
+        // double => 64bits => (precisão dupla)
+
+        // var teste = "legal";
+        boolean testes = true;
+
+        var numero = 20;
+
+        var nomeDoMeio = "levi utima";
+
+
+        if(nomeDoMeio == "levi utima") {
+            System.err.println("Boa, isso mesmo");
+        } else {
+            System.err.println("Vacilou kkkkkk");
+        }
+
+    }
+
+}
